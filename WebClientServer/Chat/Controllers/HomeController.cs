da259e2fddc9c197ba75852ea979432baca83b2d
AresDev <armando.restrepo@gmail.com>
2014-02-07 18:24:35 -0500

    se agrega el proyecto server y cliente

---
+++ WebClientServer/Chat/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Chat.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+
+        public ActionResult Chat()
+        {
+            return View();
+        }
+
+        public ActionResult Board()
+        {
+            return View();
+        }
+    }
+}
